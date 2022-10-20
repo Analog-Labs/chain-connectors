8ec82d6174eec3dec2e2560756ad9189eeffa77c
David Craven <david@craven.ch>
2022-10-20 12:39:59 +0200

    Initial commit.

---
+++ dioxus-wallet/src/lib.rs
@@ -0,0 +1,36 @@
+use dioxus::prelude::*;
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub extern "C" fn start_app() {
+    use wry::android_binding;
+
+    std::env::set_var("RUST_BACKTRACE", "1");
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_min_level(log::Level::Trace)
+            .with_tag("dioxus_wallet"),
+    );
+
+    android_binding!(com_example, dioxus_1wallet, _start_app);
+}
+
+#[cfg(target_os = "android")]
+fn _start_app() {
+    if let Err(err) = std::panic::catch_unwind(std::panic::AssertUnwindSafe(main)) {
+        eprintln!("attempt to unwind out of `rust` with err: {:?}", err);
+        std::process::abort();
+    }
+}
+
+pub fn main() {
+    dioxus_desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "hello world!"
+        }
+    })
+}
