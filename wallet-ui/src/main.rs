ed4352dce67402f14df4e70bcb3ecb66aef789e4
aliTanveerInvoZone <ali.tanveer@invozone.com>
2022-10-28 03:16:51 +0500

    - Added send and Receive screens - copy functionality

--- wallet-ui/src/main.rs
+++ wallet-ui/src/main.rs
@@ -1,7 +1,5 @@
-use std::collections::{binary_heap, HashMap};
-
+use arboard::Clipboard;
 use dioxus::{
-    core::exports::bumpalo::collections::vec,
     prelude::*,
     router::{Route, Router},
 };
@@ -35,20 +33,29 @@ static APP: Component<()> = |cx| {
 pub fn Dashboard(cx: Scope) -> Element {
     let dummyAssets = ["Eth", "Bitcoin", "Dot"];
 
-    let balance = use_state(&cx, || 0.00);
+    let balance = use_state(&cx, || 2.30);
+    let account_address = use_state(&cx, || {
+        "0x853Be3012eCeb1fC9Db70ef0Dc85Ccf3b63994BE".to_string()
+    });
 
     cx.render(rsx!(
         div {
                 class:"main-container",
         div{
         class: "dashboard-container" ,
-        h2{
-            class:"chain-name",
+        h1{
+            class:"screen-title",
+
             "Ethereum"
         }
-        div{
-            class: "balance-container",
-            "${balance}",
+
+        h5{
+            "{account_address}"
+        }
+        h2{
+
+
+            "{balance} ETH",
         }
         div {
             class:"button-container",
@@ -86,10 +93,49 @@ pub fn Dashboard(cx: Scope) -> Element {
 //-------- Send Component -------- //
 
 pub fn SendScreen(cx: Scope) -> Element {
+    let sender_address = use_state(&cx, || {
+        "0x853Be3012eCeb1fC9Db70ef0Dc85Ccf3b63994BE".to_string()
+    });
+    let amount = use_state(&cx, || "".to_string());
+    println!("{:?}", amount);
     cx.render(rsx! {
       div{
-          "send Screen",
-          Link{to:"/", "goback"}
+        class:"main-container",
+            div{
+                class:"back-button-container",
+                Link{
+                    class:"back-button"
+                    to:"/",
+                    "X"
+                         },
+                     },
+                h2{"SEND ETHEREUM"}
+                div{
+                    class:"input-container",
+                    input{
+                        class:"input",
+                        "type":"text",
+                        value:"{sender_address}",
+                        placeholder:"Recipient Address",
+                        oninput: move |evt| sender_address.set(evt.value.clone()),
+                    }
+                    input{
+                        class:"input",
+                        value:"{amount}",
+                        placeholder:"ETH Amount",
+                        // oninput:move |evt| println!("{:?}",evt.value.parse::<f64>())
+                        oninput: move |evt| match evt.value.clone().parse::<f64>(){
+                            Ok(_) =>{ amount.set(evt.value.clone()) }
+                            Err(e) => {amount.set("".to_string());
+                                            println!("invalid Input {:?}",e);}
+                        },
+                    }
+                    button{
+                        class:"button",
+                        onclick: move |evt| println!("{:?} {:?}", sender_address, amount ),
+                        "SEND",
+                    }
+                }
       }
     })
 }
@@ -97,12 +143,52 @@ pub fn SendScreen(cx: Scope) -> Element {
 // ------Receive component------ //
 
 pub fn ReceiveScreen(cx: Scope) -> Element {
+    let account_address = use_state(&cx, || {
+        "0x853Be3012eCeb1fC9Db70ef0Dc85Ccf3b63994BE".to_string()
+    });
     cx.render(rsx! {
-      div{
-          "Receive Screen",
-          Link{to:"/", "goback"}
-      }
+        div{
+        class:"main-container",
+     div{
+                class:"back-button-container",
+                Link{
+                    class:"back-button"
+                    to:"/",
+                    "X"
+                         },
+                     },
+
+                    h2{"RECEIVE ETHEREUM"},
+                    h4{"Copy and share account address."}
+                    h6{
+                        "i.e Send only Eth to this address."
+                    }
+
+                    div{
+                        class:"input-container",
+                        input{
+                            class:"input",
+                            value:"{account_address}",
+                            disabled:"true",
+                        }
+
+                        button{
+                            class:"button",
+                            onclick:move |evt| copy_to_clipboard(account_address.to_string()),
+                            "COPY"
+                        }
+
+                    }
+        }
     })
 }
 
 //------------------------------------//
+
+// ------ Utilities --------  //
+
+fn copy_to_clipboard(string: String) {
+    let mut clipboard = Clipboard::new().unwrap();
+    clipboard.set_text(string).unwrap();
+    println!("copied Text is: \"{:?}\"", clipboard.get_text().unwrap());
+}
