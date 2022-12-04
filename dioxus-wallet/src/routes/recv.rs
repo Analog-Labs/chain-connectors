633299f17099ff031db4459388527bfadae7e1c8
David Craven <david@craven.ch>
2022-12-04 19:08:02 +0100

    Rework syncing.

--- dioxus-wallet/src/routes/recv.rs
+++ dioxus-wallet/src/routes/recv.rs
@@ -1,17 +1,14 @@
 use crate::qrcode::Qrcode;
 use crate::state::use_chain_from_route;
-use crate::worker::Action;
 use dioxus::prelude::*;
 use dioxus_router::Link;
 
 #[allow(non_snake_case)]
 #[inline_props]
 pub fn Recv(cx: Scope) -> Element {
-    let handle = use_coroutine_handle(&cx).unwrap();
     let chain = use_chain_from_route(&cx);
     let info = chain.info();
     let state = chain.use_state(&cx).read();
-    handle.send(Action::SyncAccount(info.chain));
     let qrcode = format!("{};{}", info.chain, &state.account);
     cx.render(rsx! {
         div {
--- dioxus-wallet/src/worker.rs
+++ dioxus-wallet/src/worker.rs
@@ -4,46 +4,41 @@ use anyhow::{Error, Result};
 use dioxus::prelude::*;
 use fermi::*;
 use futures::channel::mpsc::UnboundedReceiver;
-use futures::stream::StreamExt;
-use rosetta_client::{Signer, Wallet};
-use std::collections::btree_map::{BTreeMap, Entry};
+use rosetta_client::Wallet;
+use std::time::Duration;
 
-pub enum Action {
-    SyncAccount(Chain),
-    SyncBalance(Chain),
-}
-
-pub fn use_worker(cx: &Scope) {
-    let state = State::new(cx);
-    use_coroutine(cx, |rx| worker(rx, state));
+pub fn use_chain_workers(cx: &Scope) -> Result<()> {
+    let keyfile = rosetta_client::default_keyfile()?;
+    let signer = rosetta_client::open_or_create_keyfile(&keyfile)?;
+    for (chain, _) in CHAINS.iter() {
+        let state = State::new(cx, *chain);
+        let wallet = Wallet::new(chain.url(), chain.config(), &signer)?;
+        state.set_account(wallet.account().address.clone());
+        use_coroutine(cx, |_: UnboundedReceiver<()>| chain_worker(state, wallet));
+    }
+    Ok(())
 }
 
 #[derive(Clone)]
 struct State {
-    chains: BTreeMap<Chain, UseAtomRef<ChainState>>,
+    chain: UseAtomRef<ChainState>,
     alerts: UseAtomRef<Vec<Alert>>,
 }
 
 impl State {
-    fn new(cx: &Scope) -> Self {
+    fn new(cx: &Scope, chain: Chain) -> Self {
         let alerts = use_atom_ref(cx, ALERTS).clone();
-        let chains = CHAINS
-            .iter()
-            .map(|(chain, handle)| (*chain, handle.use_state(cx).clone()))
-            .collect();
-        Self { alerts, chains }
+        let handle = CHAINS.get(&chain).unwrap();
+        let chain = handle.use_state(cx).clone();
+        Self { alerts, chain }
     }
 
-    fn chain(&self, chain: Chain) -> &UseAtomRef<ChainState> {
-        self.chains.get(&chain).unwrap()
+    fn set_balance(&self, balance: String) {
+        self.chain.write().balance = balance;
     }
 
-    fn set_balance(&self, chain: Chain, balance: String) {
-        self.chain(chain).write().balance = balance;
-    }
-
-    fn set_account(&self, chain: Chain, account: String) {
-        self.chain(chain).write().account = account;
+    fn set_account(&self, account: String) {
+        self.chain.write().account = account;
     }
 
     fn add_error(&self, error: Error) {
@@ -51,53 +46,25 @@ impl State {
     }
 }
 
-struct Chains {
-    signer: Signer,
-    chains: BTreeMap<Chain, Wallet>,
+async fn chain_worker(state: State, wallet: Wallet) {
+    loop {
+        if let Err(error) = fallible_chain_worker(&state, &wallet).await {
+            state.add_error(error);
+            async_std::task::sleep(Duration::from_secs(10)).await;
+        }
+    }
 }
 
-impl Chains {
-    fn new() -> Result<Self> {
-        let keyfile = rosetta_client::default_keyfile()?;
-        let signer = rosetta_client::open_or_create_keyfile(&keyfile)?;
-        Ok(Self {
-            signer,
-            chains: Default::default(),
-        })
-    }
-
-    async fn wallet(&mut self, chain: Chain) -> Result<&Wallet> {
-        if let Entry::Vacant(entry) = self.chains.entry(chain) {
-            let wallet = Wallet::new(chain.url(), chain.config(), &self.signer)?;
-            entry.insert(wallet);
+async fn fallible_chain_worker(state: &State, wallet: &Wallet) -> Result<()> {
+    let synced = None;
+    loop {
+        let current = Some(wallet.status().await?);
+        if current == synced {
+            async_std::task::sleep(Duration::from_secs(10)).await;
+            continue;
         }
-        Ok(self.chains.get(&chain).unwrap())
-    }
-
-    async fn account(&mut self, chain: Chain) -> Result<String> {
-        let wallet = self.wallet(chain).await?;
-        Ok(wallet.account().address.clone())
-    }
-
-    async fn balance(&mut self, chain: Chain) -> Result<String> {
-        let wallet = self.wallet(chain).await?;
         let amount = wallet.balance().await?;
-        rosetta_client::amount_to_string(&amount)
-    }
-}
-
-async fn worker(mut rx: UnboundedReceiver<Action>, state: State) {
-    let mut chains = Chains::new().unwrap();
-    while let Some(action) = rx.next().await {
-        match action {
-            Action::SyncBalance(chain) => match chains.balance(chain).await {
-                Ok(balance) => state.set_balance(chain, balance),
-                Err(error) => state.add_error(error),
-            },
-            Action::SyncAccount(chain) => match chains.account(chain).await {
-                Ok(account) => state.set_account(chain, account),
-                Err(error) => state.add_error(error),
-            },
-        }
+        let balance = rosetta_client::amount_to_string(&amount)?;
+        state.set_balance(balance);
     }
 }
