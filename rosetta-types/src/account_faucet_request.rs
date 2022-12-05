f53643a203459b2d0611648da026fad778f330b1
David Craven <david@craven.ch>
2022-12-05 17:31:33 +0100

    Add faucet.

--- rosetta-types/src/account_faucet_request.rs
+++ rosetta-types/src/account_faucet_request.rs
@@ -1,25 +1,27 @@
+use crate::{AccountIdentifier, NetworkIdentifier};
+
 /// AccountFaucetRequest : AccountFaucetRequest is sent for faucet on an account.
 #[derive(Clone, Debug, PartialEq, Eq, Default, Serialize, Deserialize)]
 pub struct AccountFaucetRequest {
     #[serde(rename = "network_identifier")]
-    pub network_identifier: crate::NetworkIdentifier,
-    #[serde(rename = "account_address")]
-    pub account_address: String,
-    #[serde(rename = "amount")]
-    pub amount: u128,
+    pub network_identifier: NetworkIdentifier,
+    #[serde(rename = "account_identifier")]
+    pub account_identifier: AccountIdentifier,
+    #[serde(rename = "faucet_parameter")]
+    pub faucet_parameter: u128,
 }
 
 impl AccountFaucetRequest {
     /// AccountCoinsRequest is utilized to make a request on the /account/coins endpoint.
     pub fn new(
-        network_identifier: crate::NetworkIdentifier,
-        account_address: String,
-        amount: u128,
+        network_identifier: NetworkIdentifier,
+        account_identifier: AccountIdentifier,
+        faucet_parameter: u128,
     ) -> AccountFaucetRequest {
         AccountFaucetRequest {
             network_identifier,
-            account_address,
-            amount,
+            account_identifier,
+            faucet_parameter,
         }
     }
 }
