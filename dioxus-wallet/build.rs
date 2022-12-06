c0bee14237d080bbb668f491496e4f75bc53863d
David Craven <david@craven.ch>
2022-12-06 15:50:11 +0100

    Cleanup build.rs

--- dioxus-wallet/build.rs
+++ dioxus-wallet/build.rs
@@ -5,20 +5,9 @@ fn main() {
         println!("cargo:rustc-link-lib=static:+whole-archive,-bundle=qrcodescanner");
         println!("cargo:rustc-link-search=native={}", out_dir);
 
-        //println!("cargo:rustc-link-lib=framework=MLKitVision");
-        //println!("cargo:rustc-link-lib=framework=MLKitBarcodeScanning");
-        //println!("cargo:rustc-link-search=framework=/home/dvc/rosetta-wallet/dioxus-wallet/swift/Pods/MLKitBarcodeScanning/Frameworks");
-        //println!("cargo:rustc-link-search=framework=/home/dvc/rosetta-wallet/dioxus-wallet/swift/Pods/MLKitVision/Frameworks");
         cc::Build::new()
             .cargo_metadata(false)
             .flag("-fmodules")
-            //.link_lib_modifier("+whole-archive")
-            //.cpp_link_stdlib("stdc++")
-            //.include("swift/Pods/Headers/Public")
-            .include("swift/Pods/MLKitVision/Frameworks/MLKitVision.framework/Headers")
-            .include(
-                "swift/Pods/MLKitBarcodeScanning/Frameworks/MLKitBarcodeScanning.framework/Headers",
-            )
             .file("QrCodeScanner.m")
             .compile("qrcodescanner");
     }
