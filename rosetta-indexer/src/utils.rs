fc14f4ab8a91772bceac039f122adfcff2e0840b
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2022-12-16 20:16:38 +0500

    improving code sturcture for indexer

--- rosetta-indexer/src/utils.rs
+++ rosetta-indexer/src/utils.rs
@@ -1,7 +1,7 @@
 use rosetta_client::Client;
 use rosetta_types::{
-    AccountIdentifier, BlockIdentifier, BlockRequest, BlockResponse, BlockTransaction,
-    NetworkIdentifier, PartialBlockIdentifier, Transaction, TransactionIdentifier,
+    AccountIdentifier, Amount, BlockIdentifier, BlockRequest, BlockResponse, BlockTransaction,
+    Currency, NetworkIdentifier, PartialBlockIdentifier, Transaction, TransactionIdentifier,
 };
 use surf::Body;
 use tide::Response;
@@ -14,6 +14,7 @@ pub struct TxIndexerProps {
     pub operation_type: Option<String>,
     pub address: Option<String>,
     pub success: Option<bool>,
+    pub currency: Option<Currency>,
 }
 
 pub async fn get_indexed_transactions(
@@ -61,10 +62,9 @@ pub fn filter_tx(
     if let Some(block) = block_response.block.clone() {
         for tx in block.transactions {
             let mut vec_of_operations = vec![];
-            if let Some(ref tx_id) = req.transaction_identifier {
-                if !tx_id.hash.eq(&tx.transaction_identifier.hash) {
-                    continue;
-                }
+
+            if !match_tx_id(&req.transaction_identifier, &tx.transaction_identifier.hash) {
+                continue;
             }
 
             let last_event = match tx.operations.last() {
@@ -72,41 +72,21 @@ pub fn filter_tx(
                 None => continue,
             };
 
-            if let Some(success_status) = req.success {
-                if success_status {
-                    if !last_event.r#type.ends_with(".ExtrinsicSuccess") {
-                        continue;
-                    }
-                } else if !last_event.r#type.ends_with(".ExtrinsicFailed") {
-                    continue;
-                }
+            if !match_success(&last_event.r#type, req.success) {
+                continue;
             }
 
             for op in tx.operations {
-                if let Some(operation_type) = req.operation_type.as_ref() {
-                    if !op.r#type.ends_with(operation_type) {
-                        continue;
-                    }
+                if !match_operation_type(&req.operation_type, &op.r#type) {
+                    continue;
                 }
 
-                if let Some(acc_identifier) = req.account_identifier.as_ref() {
-                    let filter_address = acc_identifier.address.trim_start_matches("0x");
-                    if let Some(op_identifier) = op.account.clone() {
-                        let address_match = if filter_address.eq(&op_identifier.address) {
-                            true
-                        } else {
-                            match op_identifier.sub_account.as_ref() {
-                                Some(sub_address) => filter_address.eq(&sub_address.address),
-                                None => false,
-                            }
-                        };
+                if !match_address(&req.account_identifier, &op.account) {
+                    continue;
+                };
 
-                        if !address_match {
-                            continue;
-                        }
-                    } else {
-                        continue;
-                    }
+                if !match_currency(&req.currency, &op.amount){
+                    continue;
                 }
 
                 vec_of_operations.push(op);
@@ -137,4 +117,71 @@ pub fn string_to_err_response(err: String) -> tide::Result {
     Ok(Response::builder(500)
         .body(Body::from_json(&error)?)
         .build())
+}
+
+pub fn match_tx_id(tx_identifier: &Option<TransactionIdentifier>, received_tx: &String) -> bool {
+    if let Some(ref tx_id) = tx_identifier {
+        tx_id.hash.eq(received_tx)
+    } else {
+        true
+    }
+}
+
+pub fn match_success(tx_success: &String, received_success: Option<bool>) -> bool {
+    if let Some(success) = received_success {
+        if success {
+            tx_success.to_lowercase().contains("success")
+        } else {
+            tx_success.to_lowercase().contains("fail")
+        }
+    } else {
+        true
+    }
+}
+
+pub fn match_operation_type(op_type: &Option<String>, received_type: &String) -> bool {
+    if let Some(operation_type) = op_type.as_ref() {
+        received_type
+            .to_lowercase()
+            .ends_with(&operation_type.to_lowercase())
+    } else {
+        true
+    }
+}
+
+pub fn match_address(
+    received_acc_identifier: &Option<AccountIdentifier>,
+    op_acc_identifier: &Option<AccountIdentifier>,
+) -> bool {
+    if let Some(acc_identifier) = received_acc_identifier.as_ref() {
+        let filter_address = acc_identifier.address.trim_start_matches("0x");
+        if let Some(op_identifier) = op_acc_identifier.clone() {
+            let address_match = if filter_address.eq(&op_identifier.address) {
+                true
+            } else {
+                match op_identifier.sub_account.as_ref() {
+                    Some(sub_address) => filter_address.eq(&sub_address.address),
+                    None => false,
+                }
+            };
+            !address_match
+        } else {
+            return false;
+        }
+    } else {
+        return true;
+    }
+}
+
+pub fn match_currency(received_curreny: &Option<Currency>, op_amount: &Option<Amount>) -> bool {
+    if let Some(currency) = received_curreny {
+        if let Some(amount) = op_amount {
+            currency.symbol.eq(&amount.currency.symbol)
+                && currency.decimals == amount.currency.decimals
+        } else {
+            true
+        }
+    } else {
+        true
+    }
 }
