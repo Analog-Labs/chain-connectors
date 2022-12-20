f02e7c60358f1acc8a01f5e3c34e94d488b511fb
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2022-12-20 19:22:27 +0500

    fixing docker port issue

--- rosetta-indexer/src/args.rs
+++ rosetta-indexer/src/args.rs
@@ -3,10 +3,10 @@ use rosetta_client::Chain;
 
 #[derive(Parser)]
 pub struct IndexerArgs {
+    #[clap(long, default_value="http://127.0.0.1:8083")]
+    pub url: String,
     #[clap(long)]
-    pub port: String,
-    #[clap(long)]
-    pub url: Option<String>,
+    pub server: Option<String>,
     #[clap(long)]
     pub chain: Chain,
 }
