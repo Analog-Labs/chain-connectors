2cabde978e008f48620bdf8b57d039839e1a9301
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2022-12-20 19:23:51 +0500

    fixing fmt

--- rosetta-indexer/src/args.rs
+++ rosetta-indexer/src/args.rs
@@ -3,7 +3,7 @@ use rosetta_client::Chain;
 
 #[derive(Parser)]
 pub struct IndexerArgs {
-    #[clap(long, default_value="http://127.0.0.1:8083")]
+    #[clap(long, default_value = "http://127.0.0.1:8083")]
     pub url: String,
     #[clap(long)]
     pub server: Option<String>,
