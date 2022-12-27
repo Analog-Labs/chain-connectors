58941c3270d9ef6c15fe43fa1729a1ff0da21784
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2022-12-27 17:51:49 +0500

    updating code

--- rosetta-types/src/runtime_call_request.rs
+++ rosetta-types/src/runtime_call_request.rs
@@ -1,12 +1,14 @@
 use serde_json::Value;
 
-use crate::{NetworkIdentifier};
+use crate::NetworkIdentifier;
 
 /// AccountFaucetRequest : AccountFaucetRequest is sent for faucet on an account.
 #[derive(Clone, Debug, PartialEq, Eq, Default, Serialize, Deserialize)]
 pub struct RuntimeCallRequest {
     #[serde(rename = "network_identifier")]
     pub network_identifier: NetworkIdentifier,
+    #[serde(rename = "pallet_name")]
+    pub pallet_name: String,
     #[serde(rename = "call_name")]
     pub call_name: String,
     #[serde(rename = "params")]
@@ -17,11 +19,13 @@ impl RuntimeCallRequest {
     /// AccountCoinsRequest is utilized to make a request on the /account/coins endpoint.
     pub fn new(
         network_identifier: NetworkIdentifier,
+        pallet_name: String,
         call_name: String,
         params: Value,
     ) -> RuntimeCallRequest {
         RuntimeCallRequest {
             network_identifier,
+            pallet_name,
             call_name,
             params,
         }
