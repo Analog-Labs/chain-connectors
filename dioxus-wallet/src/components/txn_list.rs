287bbcc0b93734ba8b09b9067e9c540f6aefe986
aliTanveerAnalog <tanveer@analog.one>
2023-01-10 15:02:51 +0500

    Clippy fixes

--- dioxus-wallet/src/components/txn_list.rs
+++ dioxus-wallet/src/components/txn_list.rs
@@ -11,14 +11,13 @@ pub fn TransactionList(
     chain: Chain,
 ) -> Element {
     cx.render(rsx! {
-            transactions.iter().map(|tx| {
+          transactions.iter().map(|tx| {
                if let Some(metadata) = tx.transaction.metadata.clone(){
                     let sender = metadata["from"].clone();
                     let amount = metadata["amount"].as_str().unwrap();
                     let is_sender = chain_state.account.eq(&sender);
                     let converted_amount = convert_to_highest_unit(
-                      amount.to_string(),
-                      chain.clone()).unwrap_or_else(|_|{"0".to_string()});
+                      amount.to_string(), *chain).unwrap_or_else(|_|{"0".to_string()});
                     let (title,address,icon) = match is_sender {
                           true => (
                             "SENT",
@@ -31,7 +30,7 @@ pub fn TransactionList(
                             img!("receive-txn.png")
                           )
                     };
-                     rsx! {
+                    rsx! {
                           div {
                               class: "transaction-item",
                               div {
@@ -41,25 +40,26 @@ pub fn TransactionList(
                                   src: icon,
                                 }
                               }
-                          div {
-                              class: "tx-content-container",
                               div {
-                                class: "tx-title",
-                                "{title}"
+                                class: "tx-content-container",
+                                div {
+                                  class: "tx-title",
+                                  "{title}"
+                                }
+                                div {
+                                  class: "txn-address",
+                                  "{address}"
+                                }
                               }
                               div {
-                                class: "txn-address",
-                                "{address}"
-                            }
+                                class: "tx-amount",
+                                "{converted_amount}"
+                              }
                           }
-                          div {
-                            class: "tx-amount",
-                            "{converted_amount}"
-                          }
-                        }
-          }
-        }
-        else {rsx!({})}
+                    }
+                } else {
+                   rsx!{{}}
+                }
+            })
     })
-        })
 }
