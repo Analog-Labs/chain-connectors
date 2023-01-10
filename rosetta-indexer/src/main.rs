a191f61a0b18816689dc3b8bc98ce4e34bca79b2
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-01-11 01:30:37 +0500

    adding cors in rosetta-indexer

--- rosetta-indexer/src/main.rs
+++ rosetta-indexer/src/main.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 use clap::Parser;
 use rosetta_indexer::args::IndexerArgs;
 use rosetta_indexer::server;
+use tide::http::headers::HeaderValue;
+use tide::security::{CorsMiddleware, Origin};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -11,8 +13,14 @@ async fn main() -> Result<()> {
 
     let chain = opts.chain;
 
+    let cors = CorsMiddleware::new()
+        .allow_methods("POST".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+
     let mut app = tide::new();
     app.with(tide::log::LogMiddleware::new());
+    app.with(cors);
     app.at("/").nest(server(chain, opts.server).await?);
     app.listen(opts.url).await?;
     Ok(())
