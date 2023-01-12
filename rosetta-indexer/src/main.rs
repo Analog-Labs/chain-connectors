8787ec59db5fcaffa5bca34a951f6dc8cd76f2fa
David Craven <david@craven.ch>
2023-01-12 09:58:34 +0100

    Some cleanup.

--- rosetta-indexer/src/main.rs
+++ rosetta-indexer/src/main.rs
@@ -1,17 +1,25 @@
 use anyhow::Result;
 use clap::Parser;
-use rosetta_indexer::args::IndexerArgs;
+use rosetta_client::Chain;
 use rosetta_indexer::server;
 use tide::http::headers::HeaderValue;
 use tide::security::{CorsMiddleware, Origin};
+
+#[derive(Parser)]
+pub struct IndexerArgs {
+    #[clap(long, default_value = "http://127.0.0.1:8083")]
+    pub url: String,
+    #[clap(long)]
+    pub server: Option<String>,
+    #[clap(long)]
+    pub chain: Chain,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     femme::start();
 
     let opts = IndexerArgs::parse();
-
-    let chain = opts.chain;
 
     let cors = CorsMiddleware::new()
         .allow_methods("POST".parse::<HeaderValue>().unwrap())
@@ -21,7 +29,7 @@ async fn main() -> Result<()> {
     let mut app = tide::new();
     app.with(tide::log::LogMiddleware::new());
     app.with(cors);
-    app.at("/").nest(server(chain, opts.server).await?);
+    app.at("/").nest(server(opts.chain, opts.server).await?);
     app.listen(opts.url).await?;
     Ok(())
 }
