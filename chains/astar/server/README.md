cde4329857cd4226875bcf56d478f2112afbd95c
David Craven <david@craven.ch>
2023-06-21 09:32:05 +0200

    Add astar support.

---
+++ chains/astar/server/README.md
@@ -0,0 +1,167 @@
+# __Rosetta Server for Substrate Chains__
+
+This Project contains `BlockchainClient` implementation of substrate chains.
+
+Methods implemented are:
+* `config`
+* `genesis_block`
+* `node_version`
+* `current_block`
+* `balance`
+* `faucet`
+* `metadata`
+* `submit`
+* `block`
+* `block_transaction`
+* `call`
+
+
+### `config`:
+
+This method returns `BlockchainConfig` which contains the configuration specific details for polkadot chain.
+
+### `genesis_block`:
+
+Returns genesis block identifier.
+
+### `node_version`:
+
+Returns node client version.
+
+### `current_block`:
+
+Fetches current block using RPC and returns its identifier.
+
+### `balance`:
+
+Fetches account balance from on chain and returns it. It takes two arguments:
+`address`: Address of account we want to fetch balance of.
+`block`: block identifier of block at which we want to fetch balance of account.
+
+### `block`:
+
+This function takes `PartialBlockIdentifier` which contains a block index or hash and returns block transaction and operations happened in that transaction.
+
+### `block_transaction`:
+
+This function takes: 
+`block`: Which is a block identifier of block from which we want to fetch transaction from.
+`tx`: Transaction identifier of transaction we want to fetch.
+And returns a specific transaction and its operations within specified block.
+
+### `faucet`:
+
+This method is used to fund an account with some amount of tokens in testnet. It takes two arguments:
+`address`: Address of account we want to fund.
+`amount`: Amount of tokens we want to fund.
+
+### `metadata`:
+
+This call is used to fetch nonce of account, It takes two arguments:
+`public_key`: This is the public key of sender.
+`options`: This is Params needed to create metadata. For polkadot chain it takes
+    `pallet_name`: name of pallet.
+    `call_name`: function of pallet.
+    `call_args`: parameters of pallet.
+
+It returns `PolkadotMetadata` which includes `nonce`, `runtime specs`, `genesis hash` and pallet related information.
+
+### `submit`:
+
+It takes transaction bytes which is signed transaction bytes and it Submits signed transaction to chain and return its transaction id.
+
+
+### `call`:
+
+To fetch Storage or any Constant from a Substrate chain, you can use the `Call` function. This method takes a `CallRequest` as input and returns a json `Value` as output. The `CallRequest` contains the following fields:
+* `NetworkIdentifier` - The network to make the call on.
+* `Method` - A string contains `-` seperated the name of pallet, function of pallet and type of query e.g. `Storage`, `Constant`. 
+* `Parameters` - A Array containing the parameters required for that pallet function.
+
+### __Passing paramters__
+`Enums` and `Struct` are passed in paramter as array values
+lets say we have an enum:
+```Rust
+enum TestEnum {
+    A,
+    B(u32),
+    C,
+}
+```
+and we want to pass `TestEnum::A` as parameter then we pass `["A", ""]` as parameter.
+Second value in array is for the paramter of enum variant. In this case its will be empty since enum does not contain any paramter.
+if we want to pass `TestEnum::B(10)` as parameter then we pass `["B", 10]` as parameter. 
+Here we passed 10 as a Json Number in array.
+
+Now lets take a look if a function call requires a some complex enum like:
+```Rust
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, Debug)]
+pub enum MultiAddress<AccountId, AccountIndex> {
+    /// It's an account ID (pubkey).
+    Id(AccountId),
+    /// It's an account index.
+    Index(#[codec(compact)] AccountIndex),
+    /// It's some arbitrary raw bytes.
+    Raw(Vec<u8>),
+    /// It's a 32 byte representation.
+    Address32([u8; 32]),
+    /// Its a 20 byte representation.
+    Address20([u8; 20]),
+}
+```
+we need to pass Id variant of this enum now this variant takes a parameter which is `AccountId` type. Type of `AccountId` is 
+
+```Rust
+type AccountId = AccountId32;
+```
+where `AccountId32` is a struct with 32 byte vector.
+
+```Rust
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, Debug)]
+pub struct AccountId32(pub [u8; 32]);
+```
+
+So we need to pass `AccountId32` as a parameter. We can pass it as `[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]` which is a 32 byte array. and complete enum param will look like this
+
+```Json
+{
+    "params": [
+        ["Id", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    ]
+}
+
+```
+
+Similarly if we want to pass a `Struct` in parameters then we pass it as array of values as well.
+let's say we have a struct:
+
+```Rust
+struct TestStruct {
+    a: u32,
+    b: u32,
+    c: u32,
+}
+```
+and we want to pass `TestStruct { a: 1, b: 2, c: 3 }` as parameter then we pass `[1, 2, 3]` as parameter array.
+So passing those both as a paramter list should look like this
+```Json
+{
+    "method": "pallet_name-storage_name-query_type",
+    "params": [
+        ["A", ""], //representing TestEnum::A
+        [1, 2, 3] //representing TestStruct { a: 1, b: 2, c: 3 }
+    ]
+}
+```
+
+Other primitive types are passed as they are. e.g.
+```Json
+{
+    "method": "pallet_name-storage_name-query_type",
+    "params": [
+        1,          //representing u32
+        "test",     //representing String
+        false,      //representing bool
+    ]
+}
+```
---
+++ chains/astar/server/src/lib.rs
@@ -0,0 +1,352 @@
+use anyhow::{Context, Result};
+use parity_scale_codec::{Decode, Encode};
+use rosetta_config_astar::{AstarMetadata, AstarMetadataParams};
+use rosetta_server::crypto::address::Address;
+use rosetta_server::crypto::PublicKey;
+use rosetta_server::types::{
+    Block, BlockIdentifier, CallRequest, Coin, PartialBlockIdentifier, Transaction,
+    TransactionIdentifier,
+};
+use rosetta_server::{BlockchainClient, BlockchainConfig};
+use serde_json::Value;
+use sp_keyring::AccountKeyring;
+use std::time::Duration;
+use subxt::config::{Hasher, Header};
+use subxt::metadata::DecodeStaticType;
+use subxt::rpc::types::BlockNumber;
+use subxt::storage::address::{StorageHasher, StorageMapKey, Yes};
+use subxt::storage::StaticStorageAddress;
+use subxt::tx::{PairSigner, StaticTxPayload, SubmittableExtrinsic};
+use subxt::utils::{AccountId32, MultiAddress, H256};
+use subxt::{Config, OnlineClient, PolkadotConfig};
+
+mod block;
+mod call;
+
+pub struct AstarClient {
+    config: BlockchainConfig,
+    client: OnlineClient<PolkadotConfig>,
+    genesis_block: BlockIdentifier,
+}
+
+impl AstarClient {
+    async fn account_info(
+        &self,
+        address: &Address,
+        block: Option<&BlockIdentifier>,
+    ) -> Result<AccountInfo<u32, AccountData>> {
+        let address: AccountId32 = address
+            .address()
+            .parse()
+            .map_err(|err| anyhow::anyhow!("{}", err))
+            .context("invalid address")?;
+        let hash = self.client.metadata().storage_hash("System", "Account")?;
+        let key = StaticStorageAddress::<
+            DecodeStaticType<AccountInfo<u32, AccountData>>,
+            Yes,
+            Yes,
+            Yes,
+        >::new(
+            "System",
+            "Account",
+            vec![StorageMapKey::new(
+                &address,
+                StorageHasher::Blake2_128Concat,
+            )],
+            hash,
+        );
+
+        let block = if let Some(block) = block {
+            let block = hex::decode(&block.hash)?
+                .try_into()
+                .map_err(|_| anyhow::anyhow!("invalid block"))?;
+            Some(H256(block))
+        } else {
+            None
+        };
+        let account_info = self
+            .client
+            .storage()
+            .at(block)
+            .await?
+            .fetch_or_default(&key)
+            .await?;
+        Ok(account_info)
+    }
+}
+
+#[async_trait::async_trait]
+impl BlockchainClient for AstarClient {
+    type MetadataParams = AstarMetadataParams;
+    type Metadata = AstarMetadata;
+
+    async fn new(network: &str, addr: &str) -> Result<Self> {
+        let config = rosetta_config_astar::config(network)?;
+        let client = OnlineClient::<PolkadotConfig>::from_url(format!("ws://{addr}")).await?;
+        let genesis = client.genesis_hash();
+        let genesis_block = BlockIdentifier {
+            index: 0,
+            hash: hex::encode(genesis.as_ref()),
+        };
+        Ok(Self {
+            config,
+            client,
+            genesis_block,
+        })
+    }
+
+    fn config(&self) -> &BlockchainConfig {
+        &self.config
+    }
+
+    fn genesis_block(&self) -> &BlockIdentifier {
+        &self.genesis_block
+    }
+
+    async fn node_version(&self) -> Result<String> {
+        Ok(self.client.rpc().system_version().await?)
+    }
+
+    async fn current_block(&self) -> Result<BlockIdentifier> {
+        let block = self
+            .client
+            .rpc()
+            .block(None)
+            .await?
+            .context("no current block")?;
+        let index = block.block.header.number as _;
+        let hash = block.block.header.hash();
+        Ok(BlockIdentifier {
+            index,
+            hash: hex::encode(hash.as_ref()),
+        })
+    }
+
+    async fn balance(&self, address: &Address, block: &BlockIdentifier) -> Result<u128> {
+        let account_info = self.account_info(address, Some(block)).await?;
+        Ok(account_info.data.free)
+    }
+
+    async fn coins(&self, _address: &Address, _block: &BlockIdentifier) -> Result<Vec<Coin>> {
+        anyhow::bail!("not a utxo chain")
+    }
+
+    async fn faucet(&self, address: &Address, value: u128) -> Result<Vec<u8>> {
+        let address: AccountId32 = address
+            .address()
+            .parse()
+            .map_err(|err| anyhow::anyhow!("{}", err))
+            .context("invalid address")?;
+        let signer = PairSigner::<PolkadotConfig, _>::new(AccountKeyring::Alice.pair());
+        let dest: MultiAddress<AccountId32, u32> = MultiAddress::Id(address);
+        let tx = StaticTxPayload::new("Balances", "transfer", Transfer { dest, value }, [0; 32])
+            .unvalidated();
+        let hash = self
+            .client
+            .tx()
+            .sign_and_submit_then_watch_default(&tx, &signer)
+            .await?
+            .wait_for_finalized_success()
+            .await?
+            .extrinsic_hash();
+        Ok(hash.0.to_vec())
+    }
+
+    async fn metadata(
+        &self,
+        public_key: &PublicKey,
+        params: &Self::MetadataParams,
+    ) -> Result<Self::Metadata> {
+        let address = public_key.to_address(self.config().address_format);
+        let account_info = self.account_info(&address, None).await?;
+        let runtime = self.client.runtime_version();
+        let metadata = self.client.metadata();
+        let pallet = metadata.pallet(&params.pallet_name)?;
+        let pallet_index = pallet.index();
+        let call_index = pallet.call_index(&params.call_name)?;
+        let call_hash = metadata.call_hash(&params.pallet_name, &params.call_name)?;
+        let genesis_hash = self.client.genesis_hash().0;
+        Ok(AstarMetadata {
+            nonce: account_info.nonce,
+            spec_version: runtime.spec_version,
+            transaction_version: runtime.transaction_version,
+            genesis_hash,
+            pallet_index,
+            call_index,
+            call_hash,
+        })
+    }
+
+    async fn submit(&self, transaction: &[u8]) -> Result<Vec<u8>> {
+        let hash = SubmittableExtrinsic::from_bytes(self.client.clone(), transaction.to_vec())
+            .submit_and_watch()
+            .await?
+            .wait_for_finalized_success()
+            .await?
+            .extrinsic_hash();
+        Ok(hash.0.to_vec())
+    }
+
+    async fn block(&self, block_identifier: &PartialBlockIdentifier) -> Result<Block> {
+        let block_hash = if let Some(hash) = block_identifier.hash.as_ref() {
+            hash.parse()?
+        } else {
+            self.client
+                .rpc()
+                .block_hash(block_identifier.index.map(BlockNumber::from))
+                .await?
+                .context("block not found")?
+        };
+        let block = self.client.blocks().at(Some(block_hash)).await?;
+        let timestamp_now_address =
+            StaticStorageAddress::<DecodeStaticType<u64>, Yes, Yes, ()>::new(
+                "Timestamp",
+                "Now",
+                vec![],
+                [0; 32],
+            )
+            .unvalidated();
+        let timestamp = block
+            .storage()
+            .fetch_or_default(&timestamp_now_address)
+            .await?;
+        let body = block.body().await?;
+        let mut transactions = vec![];
+        for extrinsic in body.extrinsics() {
+            let transaction = crate::block::get_transaction(self.config(), &extrinsic).await?;
+            transactions.push(transaction);
+        }
+        Ok(Block {
+            block_identifier: BlockIdentifier {
+                index: block.number() as _,
+                hash: hex::encode(block.hash()),
+            },
+            parent_block_identifier: BlockIdentifier {
+                index: block.number().saturating_sub(1) as _,
+                hash: hex::encode(block.header().parent_hash),
+            },
+            timestamp: Duration::from_millis(timestamp).as_nanos() as i64,
+            transactions,
+            metadata: None,
+        })
+    }
+
+    async fn block_transaction(
+        &self,
+        block_identifier: &BlockIdentifier,
+        transaction_identifier: &TransactionIdentifier,
+    ) -> Result<Transaction> {
+        let block_hash = block_identifier.hash.parse()?;
+        let transaction_hash = transaction_identifier.hash.parse()?;
+        let body = self
+            .client
+            .blocks()
+            .at(Some(block_hash))
+            .await?
+            .body()
+            .await?;
+        let extrinsic = body
+            .extrinsics()
+            .find(|extrinsic| {
+                <PolkadotConfig as Config>::Hasher::hash_of(&extrinsic.bytes()) == transaction_hash
+            })
+            .context("transaction not found")?;
+        crate::block::get_transaction(self.config(), &extrinsic).await
+    }
+
+    async fn call(&self, request: &CallRequest) -> Result<Value> {
+        let call_details = request.method.split('-').collect::<Vec<&str>>();
+        if call_details.len() != 3 {
+            anyhow::bail!("Invalid length of call request params");
+        }
+        let pallet_name = call_details[0];
+        let call_name = call_details[1];
+        let query_type = call_details[2];
+        match query_type.to_lowercase().as_str() {
+            "constant" => crate::call::dynamic_constant_req(&self.client, pallet_name, call_name),
+            "storage" => {
+                crate::call::dynamic_storage_req(
+                    &self.client,
+                    pallet_name,
+                    call_name,
+                    request.parameters.clone(),
+                )
+                .await
+            }
+            _ => {
+                anyhow::bail!("invalid query type");
+            }
+        }
+    }
+}
+
+#[derive(Decode, Encode, Debug)]
+struct AccountInfo<Index, AccountData> {
+    pub nonce: Index,
+    pub consumers: Index,
+    pub providers: Index,
+    pub sufficients: Index,
+    pub data: AccountData,
+}
+
+#[derive(Decode, Encode, Debug)]
+struct AccountData {
+    pub free: u128,
+    pub reserved: u128,
+    pub misc_frozen: u128,
+    pub fee_frozen: u128,
+}
+
+#[derive(Decode, Encode, Debug)]
+pub struct Transfer {
+    pub dest: MultiAddress<AccountId32, u32>,
+    #[codec(compact)]
+    pub value: u128,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_network_list() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::network_list(config).await
+    }
+
+    #[tokio::test]
+    async fn test_network_options() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::network_options::<AstarClient>(config).await
+    }
+
+    #[tokio::test]
+    async fn test_network_status() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::network_status::<AstarClient>(config).await
+    }
+
+    #[tokio::test]
+    async fn test_account() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::account(config).await
+    }
+
+    #[tokio::test]
+    async fn test_construction() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::construction(config).await
+    }
+
+    #[tokio::test]
+    async fn test_find_transaction() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::find_transaction(config).await
+    }
+
+    #[tokio::test]
+    async fn test_list_transactions() -> Result<()> {
+        let config = rosetta_config_astar::config("dev")?;
+        rosetta_server::tests::list_transactions(config).await
+    }
+}
