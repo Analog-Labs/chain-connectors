5a5e53c59105c7b289238393721affc6a37068a6
penumbra23 <glbranimir@gmail.com>
2023-07-27 11:30:18 +0200

    Add tags for Docker images

--- build_connectors.sh
+++ build_connectors.sh
@@ -2,19 +2,19 @@
 cargo build -p rosetta-server-bitcoin --target x86_64-unknown-linux-musl --release
 mkdir -p target/release/bitcoin/bin
 cp target/x86_64-unknown-linux-musl/release/rosetta-server-bitcoin target/release/bitcoin/bin
-docker build target/release/bitcoin -f chains/bitcoin/Dockerfile -t analoglabs/connector-bitcoin
+docker build target/release/bitcoin -f chains/bitcoin/Dockerfile -t analoglabs/connector-bitcoin:latest
 
 cargo build -p rosetta-server-ethereum --target x86_64-unknown-linux-musl --release
 mkdir -p target/release/ethereum/bin
 cp target/x86_64-unknown-linux-musl/release/rosetta-server-ethereum target/release/ethereum/bin
-docker build target/release/ethereum -f chains/ethereum/Dockerfile -t analoglabs/connector-ethereum
+docker build target/release/ethereum -f chains/ethereum/Dockerfile -t analoglabs/connector-ethereum:latest
 
 cargo build -p rosetta-server-polkadot --target x86_64-unknown-linux-musl --release
 mkdir -p target/release/polkadot/bin
 cp target/x86_64-unknown-linux-musl/release/rosetta-server-polkadot target/release/polkadot/bin
-docker build target/release/polkadot -f chains/polkadot/Dockerfile -t analoglabs/connector-polkadot
+docker build target/release/polkadot -f chains/polkadot/Dockerfile -t analoglabs/connector-polkadot:latest
 
 cargo build -p rosetta-server-astar --target x86_64-unknown-linux-musl --release
 mkdir -p target/release/astar/bin
 cp target/x86_64-unknown-linux-musl/release/rosetta-server-astar target/release/astar/bin
-docker build target/release/astar -f chains/astar/Dockerfile -t analoglabs/connector-astar
+docker build target/release/astar -f chains/astar/Dockerfile -t analoglabs/connector-astar:latest
--- push_connectors.sh
+++ push_connectors.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-docker push analoglabs/connector-bitcoin
-docker push analoglabs/connector-ethereum
-docker push analoglabs/connector-polkadot
-docker push analoglabs/connector-astar
+
+# First arg is the Docker image tag on all images
+tag=$1
+
+docker push analoglabs/connector-bitcoin:latest
+docker push analoglabs/connector-ethereum:latest
+docker push analoglabs/connector-polkadot:latest
+docker push analoglabs/connector-astar:latest
+
+if [ ! -z "$tag" ]; then
+    echo "Tagging all images: $tag";
+    docker tag analoglabs/connector-bitcoin analoglabs/connector-bitcoin:$tag
+    docker push analoglabs/connector-bitcoin:$tag
+
+    docker tag analoglabs/connector-ethereum analoglabs/connector-ethereum:$tag
+    docker push analoglabs/connector-ethereum:$tag
+
+    docker tag analoglabs/connector-polkadot analoglabs/connector-polkadot:$tag
+    docker push analoglabs/connector-polkadot:$tag
+
+    docker tag analoglabs/connector-astar analoglabs/connector-astar:$tag
+    docker push analoglabs/connector-astar:$tag
+fi
+
