5db76ac0740941ad397bfc9b6531bac9382b7cf6
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-07-29 12:08:23 -0300

    Add astar config

--- chains/ethereum/config/src/lib.rs
+++ chains/ethereum/config/src/lib.rs
@@ -7,35 +7,71 @@ use std::sync::Arc;
 
 pub fn config(network: &str) -> Result<BlockchainConfig> {
     anyhow::ensure!(network == "dev");
-    Ok(BlockchainConfig {
-        blockchain: "ethereum",
-        network: "dev",
-        algorithm: Algorithm::EcdsaRecoverableSecp256k1,
-        address_format: AddressFormat::Eip55,
-        coin: 1,
-        bip44: true,
-        utxo: false,
-        currency_unit: "wei",
-        currency_symbol: "ETH",
-        currency_decimals: 18,
-        node_uri: NodeUri::parse("http://127.0.0.1:8545")?,
-        node_image: "ethereum/client-go:v1.10.26",
-        node_command: Arc::new(|_network, port| {
-            vec![
-                "--dev".into(),
-                "--dev.period=1".into(),
-                "--ipcdisable".into(),
-                "--http".into(),
-                "--http.addr=0.0.0.0".into(),
-                format!("--http.port={port}"),
-                "--http.vhosts=*".into(),
-                "--http.api=eth,debug,admin,txpool,web3".into(),
-            ]
-        }),
-        node_additional_ports: &[],
-        connector_port: 8081,
-        testnet: network == "dev",
-    })
+    let config = match network {
+        "dev" => BlockchainConfig {
+            blockchain: "ethereum",
+            network: "dev",
+            algorithm: Algorithm::EcdsaRecoverableSecp256k1,
+            address_format: AddressFormat::Eip55,
+            coin: 1,
+            bip44: true,
+            utxo: false,
+            currency_unit: "wei",
+            currency_symbol: "ETH",
+            currency_decimals: 18,
+            node_uri: NodeUri::parse("http://127.0.0.1:8545")?,
+            node_image: "ethereum/client-go:v1.10.26",
+            node_command: Arc::new(|_network, port| {
+                vec![
+                    "--dev".into(),
+                    "--dev.period=1".into(),
+                    "--ipcdisable".into(),
+                    "--http".into(),
+                    "--http.addr=0.0.0.0".into(),
+                    format!("--http.port={port}"),
+                    "--http.vhosts=*".into(),
+                    "--http.api=eth,debug,admin,txpool,web3".into(),
+                ]
+            }),
+            node_additional_ports: &[],
+            connector_port: 8081,
+            testnet: network == "dev",
+        },
+        "astar" => BlockchainConfig {
+            blockchain: "astar",
+            // Astar networks are listed here:
+            // https://github.com/AstarNetwork/Astar/blob/v5.15.0/bin/collator/src/command.rs#L56-L69
+            network: "astar",
+            algorithm: Algorithm::EcdsaRecoverableSecp256k1,
+            address_format: AddressFormat::Eip55,
+            coin: 810,
+            bip44: true,
+            utxo: false,
+            currency_unit: "planck",
+            currency_symbol: "ASTR",
+            currency_decimals: 18,
+            // The default RPC port is 9945
+            // https://github.com/AstarNetwork/Astar/blob/v5.15.0/bin/collator/src/command.rs#L965-L967
+            node_uri: NodeUri::parse("http://127.0.0.1:9945")?,
+            node_image: "staketechnologies/astar-collator:v5.15.0",
+            node_command: Arc::new(|network, port| {
+                vec![
+                    "astar-collator".into(),
+                    format!("--chain=astar"),
+                    "--rpc-cors=all".into(),
+                    "--rpc-external".into(),
+                    format!("--rpc-port={port}"),
+                    "--alice".into(),
+                    "--enable-evm-rpc".into(),
+                ]
+            }),
+            node_additional_ports: &[],
+            connector_port: 8083,
+            testnet: false,
+        },
+        _ => anyhow::bail!("unsupported network: {}", network),
+    };
+    Ok(config)
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
