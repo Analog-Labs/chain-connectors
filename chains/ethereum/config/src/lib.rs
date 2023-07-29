81ef9c6d5937cad14e98f83fa4958c77ce56851d
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-07-29 12:42:01 -0300

    Refactor config file

--- chains/ethereum/config/src/lib.rs
+++ chains/ethereum/config/src/lib.rs
@@ -8,12 +8,12 @@ use std::sync::Arc;
 pub fn config(network: &str) -> Result<BlockchainConfig> {
     anyhow::ensure!(network == "dev");
     let config = match network {
-        "dev" => BlockchainConfig {
+        "dev" | "mainnet" => BlockchainConfig {
             blockchain: "ethereum",
             network: "dev",
             algorithm: Algorithm::EcdsaRecoverableSecp256k1,
             address_format: AddressFormat::Eip55,
-            coin: 1,
+            coin: if network == "mainnet" { 60 } else { 1 },
             bip44: true,
             utxo: false,
             currency_unit: "wei",
@@ -21,48 +21,28 @@ pub fn config(network: &str) -> Result<BlockchainConfig> {
             currency_decimals: 18,
             node_uri: NodeUri::parse("http://127.0.0.1:8545")?,
             node_image: "ethereum/client-go:v1.10.26",
-            node_command: Arc::new(|_network, port| {
-                vec![
-                    "--dev".into(),
-                    "--dev.period=1".into(),
-                    "--ipcdisable".into(),
+            node_command: Arc::new(|network, port| {
+                let mut params = if network == "dev" {
+                    vec![
+                        "--dev".into(),
+                        "--dev.period=1".into(),
+                        "--ipcdisable".into(),
+                    ]
+                } else {
+                    vec!["--syncmode=full".into()]
+                };
+                params.extend_from_slice(&[
                     "--http".into(),
                     "--http.addr=0.0.0.0".into(),
                     format!("--http.port={port}"),
                     "--http.vhosts=*".into(),
                     "--http.api=eth,debug,admin,txpool,web3".into(),
-                ]
+                ]);
+                params
             }),
             node_additional_ports: &[],
             connector_port: 8081,
             testnet: network == "dev",
-        },
-        "mainnet" => BlockchainConfig {
-            blockchain: "ethereum",
-            network: "mainnet",
-            algorithm: Algorithm::EcdsaRecoverableSecp256k1,
-            address_format: AddressFormat::Eip55,
-            coin: 60,
-            bip44: true,
-            utxo: false,
-            currency_unit: "wei",
-            currency_symbol: "ETH",
-            currency_decimals: 18,
-            node_uri: NodeUri::parse("http://127.0.0.1:8545")?,
-            node_image: "ethereum/client-go:v1.10.26",
-            node_command: Arc::new(|_network, port| {
-                vec![
-                    "--syncmode=full".into(),
-                    "--http".into(),
-                    "--http.addr=0.0.0.0".into(),
-                    format!("--http.port={port}"),
-                    "--http.vhosts=*".into(),
-                    "--http.api=eth,debug,admin,txpool,web3".into(),
-                ]
-            }),
-            node_additional_ports: &[],
-            connector_port: 8081,
-            testnet: false,
         },
         "astar" => BlockchainConfig {
             blockchain: "astar",
