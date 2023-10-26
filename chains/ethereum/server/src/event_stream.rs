df73c1b1ddd87bfeb9c710d5db59901b824391a9
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-10-26 16:40:26 -0300

    Fix event stream panic

--- chains/ethereum/server/src/event_stream.rs
+++ chains/ethereum/server/src/event_stream.rs
@@ -1,14 +1,18 @@
+use crate::{client::EthereumClient, utils::NonPendingBlock};
 use ethers::{prelude::*, providers::PubsubClient};
 use futures_util::{future::BoxFuture, FutureExt};
-use rosetta_core::{stream::Stream, types::BlockIdentifier, BlockOrIdentifier, ClientEvent};
-use std::{pin::Pin, sync::Arc, task::Poll};
+use rosetta_core::{stream::Stream, BlockOrIdentifier, ClientEvent};
+use std::{pin::Pin, task::Poll};
 
 // Maximum number of failures in sequence before closing the stream
 const FAILURE_THRESHOLD: u32 = 10;
 
+#[pin_project::pin_project(project=EthereumEventStreamProjection)]
 pub struct EthereumEventStream<'a, P: PubsubClient> {
     /// Ethereum client
-    pub client: Arc<Provider<P>>,
+    // #[pin]
+    pub client: &'a crate::EthereumClient<P>,
+    // pub client: Arc<crate::EthereumClientProvider<P>>,
     /// Ethereum subscription for new heads
     pub new_head: Option<SubscriptionStream<'a, P, Block<H256>>>,
     /// Count the number of failed attempts to retrieve the finalized block
@@ -17,48 +21,70 @@ pub struct EthereumEventStream<'a, P: PubsubClient> {
     pub latest_block_failures: u32,
     /// Cache the best finalized block, we use this to avoid emitting two
     /// [`ClientEvent::NewFinalized`] for the same block
-    pub best_finalized_block: Option<BlockIdentifier>,
+    pub best_finalized_block: Option<NonPendingBlock>,
+    /// Cache the latest block, used for retrieve the latest finalized block
+    /// see [`BlockFinalityStrategy`]
+    pub latest_block: Option<NonPendingBlock>,
     /// Ethereum client doesn't support subscribing for finalized blocks, as workaround
     /// everytime we receive a new head, we query the latest finalized block
-    pub finalized_block_future:
-        Option<BoxFuture<'static, Result<Option<Block<TxHash>>, ProviderError>>>,
+    pub finalized_block_future: Option<BoxFuture<'a, anyhow::Result<NonPendingBlock>>>,
 }
 
-impl<'a, P> EthereumEventStream<'a, P>
+impl<P> EthereumEventStream<'_, P>
 where
     P: PubsubClient + 'static,
 {
-    pub fn new(
-        client: Arc<Provider<P>>,
+    pub fn new<'a>(
+        // client: Arc<Provider<P>>,
+        client: &'a crate::EthereumClient<P>,
         subscription: SubscriptionStream<'a, P, Block<H256>>,
-    ) -> Self {
-        Self {
+    ) -> EthereumEventStream<'a, P> {
+        EthereumEventStream {
             client,
             new_head: Some(subscription),
             finalized_block_failures: 0,
             latest_block_failures: 0,
             best_finalized_block: None,
             finalized_block_future: None,
+            latest_block: None,
         }
     }
 
-    fn finalized_block(&self) -> BoxFuture<'static, Result<Option<Block<TxHash>>, ProviderError>> {
-        // Clone client to make BoxFuture 'static
-        let client = Arc::clone(&self.client);
-        async move { client.get_block(BlockId::Number(BlockNumber::Finalized)).await }.boxed()
+    // fn finalized_block<'a>(self: Pin<&'a mut Self>) -> BoxFuture<'a,
+    // anyhow::Result<NonPendingBlock>> {     self.client.finalized_block(None).boxed()
+    //     // Clone client to make BoxFuture 'static
+    //     // let client = Arc::clone(&self.client);
+    //     // async move { client.get_block(BlockId::Number(BlockNumber::Finalized)).await }.boxed()
+    // }
+
+    // fn finalized_block<'a, 'b: 'a>(self: &mut Pin<&'b mut Self>) {
+    //     let fut = EthereumClient::finalized_block(self.client, None).boxed();
+    //     let this = self.project();
+    //     *this.finalized_block_future = Some(fut);
+    //     // (&mut *self).finalized_block_future = Some(self.client.finalized_block(None).boxed());
+    //     // Clone client to make BoxFuture 'static
+    //     // let client = Arc::clone(&self.client);
+    //     // async move { client.get_block(BlockId::Number(BlockNumber::Finalized)).await }.boxed()
+    // }
+}
+
+impl<'a, 'b, P> EthereumEventStreamProjection<'a, 'b, P>
+where
+    P: PubsubClient + 'static,
+{
+    fn finalized_block(&self) -> BoxFuture<'b, anyhow::Result<NonPendingBlock>> {
+        let latest_block_number = self.latest_block.as_ref().map(|block| block.number);
+        let client = &self.client;
+        EthereumClient::finalized_block(client, latest_block_number).boxed()
     }
+    // fn inc_finalized_failures(&mut self) {
+    //     let failures = *self.finalized_block_failures;
+    //     std::mem::replace(self.finalized_block_failures, failures + 1);
+    //     // Clone client to make BoxFuture 'static
+    //     // let client = Arc::clone(&self.client);
+    //     // async move { client.get_block(BlockId::Number(BlockNumber::Finalized)).await }.boxed()
+    // }
 }
-
-/// Converts [`Block`] to [`BlockIdentifier`]
-fn block_to_identifier(block: &Block<TxHash>) -> Result<BlockIdentifier, &'static str> {
-    let Some(number) = block.number else { return Err("block number is missing") };
-
-    let Some(hash) = block.hash else { return Err("block hash is missing") };
-
-    Ok(BlockIdentifier::new(number.as_u64(), hex::encode(hash)))
-}
-
-impl<'a, P> Unpin for EthereumEventStream<'a, P> where P: PubsubClient {}
 
 impl<P> Stream for EthereumEventStream<'_, P>
 where
@@ -67,87 +93,72 @@ where
     type Item = ClientEvent;
 
     fn poll_next(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Option<Self::Item>> {
-        let this = &mut *self;
-
         // Check if the stream is close
-        if this.new_head.is_none() && this.finalized_block_future.is_none() {
+        if self.new_head.is_none() && self.finalized_block_future.is_none() {
             return Poll::Ready(None);
         }
 
+        let this = self.project();
+
         // Query the latest finalized block
-        if let Some(mut finalized_block_future) = this.finalized_block_future.take() {
-            loop {
-                if this.finalized_block_failures >= FAILURE_THRESHOLD {
-                    return Poll::Ready(Some(ClientEvent::Close(
-                        "More than 10 failures in sequence".into(),
-                    )));
-                }
+        loop {
+            let Some(mut finalized_block_future) = this.finalized_block_future.take() else {
+                break;
+            };
+            match finalized_block_future.poll_unpin(cx) {
+                Poll::Ready(Ok(block)) => {
+                    // Reset failure counter
+                    *this.finalized_block_failures = 0;
 
-                match finalized_block_future.poll_unpin(cx) {
-                    Poll::Ready(Ok(Some(block))) => {
-                        // Convert raw block to block identifier
-                        let block_identifier = match block_to_identifier(&block) {
-                            Ok(block_identifier) => block_identifier,
-                            Err(error) => {
-                                this.finalized_block_failures += 1;
-                                tracing::error!("finalized block: {error}");
-                                break;
-                            },
-                        };
-
-                        // Reset failure counter
-                        this.finalized_block_failures = 0;
-
-                        // Skip if the finalized block is equal to the best finalized block
-                        if let Some(best_finalized_block) = this.best_finalized_block.take() {
-                            if block_identifier == best_finalized_block {
-                                tracing::debug!("finalized block unchanged");
-                                this.best_finalized_block = Some(best_finalized_block);
-                                break;
-                            }
+                    // Skip if the finalized block is equal to the best finalized block
+                    if let Some(best_finalized_block) = this.best_finalized_block.take() {
+                        if block.hash == best_finalized_block.hash {
+                            tracing::debug!("finalized block unchanged");
+                            *this.best_finalized_block = Some(best_finalized_block);
                         }
+                    }
 
-                        // Cache the new best finalized block
-                        this.best_finalized_block = Some(block_identifier.clone());
+                    // Cache the new best finalized block
+                    *this.best_finalized_block = Some(block.clone());
 
-                        // Return the best finalized block
-                        return Poll::Ready(Some(ClientEvent::NewFinalized(
-                            BlockOrIdentifier::Identifier(block_identifier),
+                    // Return the best finalized block
+                    return Poll::Ready(Some(ClientEvent::NewFinalized(
+                        BlockOrIdentifier::Identifier(block.identifier),
+                    )));
+                },
+                Poll::Ready(Err(error)) => {
+                    // Check failure count
+                    *this.finalized_block_failures += 1;
+                    tracing::error!("failed to retrieve finalized block: {error:?}");
+                    if *this.finalized_block_failures >= FAILURE_THRESHOLD {
+                        *this.finalized_block_future = None;
+                        return Poll::Ready(Some(ClientEvent::Close(
+                            "More than 10 failures in sequence".into(),
                         )));
-                    },
-                    Poll::Ready(Ok(None)) => {
-                        // Retry to retrieve the latest finalized block.
-                        this.finalized_block_future = Some(this.finalized_block());
-                        tracing::error!("finalized block not found");
-                        this.finalized_block_failures += 1;
-                        continue;
-                    },
-                    Poll::Ready(Err(error)) => {
-                        // Retry to retrieve the latest finalized block.
-                        this.finalized_block_future = Some(this.finalized_block());
-                        tracing::error!("failed to retrieve finalized block: {error:?}");
-                        this.finalized_block_failures += 1;
-                        continue;
-                    },
-                    Poll::Pending => {
-                        this.finalized_block_future = Some(finalized_block_future);
-                        break;
-                    },
-                }
+                    }
+
+                    // Retry to retrieve the latest finalized block.
+                    *this.finalized_block_future = Some(this.finalized_block());
+                    continue;
+                },
+                Poll::Pending => {
+                    *this.finalized_block_future = Some(finalized_block_future);
+                    break;
+                },
             }
         }
 
         let Some(mut new_head_stream) = this.new_head.take() else {
-            this.finalized_block_future = None;
+            *this.finalized_block_future = None;
             return Poll::Ready(None);
         };
 
         // Query new heads
         loop {
-            if this.latest_block_failures >= FAILURE_THRESHOLD {
+            if *this.latest_block_failures >= FAILURE_THRESHOLD {
                 return Poll::Ready(Some(ClientEvent::Close(
                     "More than 10 failures in sequence".into(),
                 )));
@@ -156,31 +167,34 @@ where
             match new_head_stream.poll_next_unpin(cx) {
                 Poll::Ready(Some(block)) => {
                     // Convert raw block to block identifier
-                    let block_identifier = match block_to_identifier(&block) {
-                        Ok(block_identifier) => block_identifier,
+                    let block = match NonPendingBlock::try_from(block) {
+                        Ok(block) => block,
                         Err(error) => {
-                            this.latest_block_failures += 1;
-                            tracing::error!("latest block: {error}");
+                            *this.latest_block_failures += 1;
+                            tracing::error!("[RPC BUG] invalid latest block: {error}");
                             continue;
                         },
                     };
 
                     // Reset failure counter
-                    this.latest_block_failures = 0;
+                    *this.latest_block_failures = 0;
+
+                    // Store the new latest block
+                    *this.latest_block = Some(block.clone());
 
                     // Query latest finalized block
                     if this.finalized_block_future.is_none() {
-                        this.finalized_block_future = Some(this.finalized_block());
+                        *this.finalized_block_future = Some(this.finalized_block());
                     }
 
-                    this.new_head = Some(new_head_stream);
+                    *this.new_head = Some(new_head_stream);
                     return Poll::Ready(Some(ClientEvent::NewHead(BlockOrIdentifier::Identifier(
-                        block_identifier,
+                        block.identifier,
                     ))));
                 },
                 Poll::Ready(None) => return Poll::Ready(None),
                 Poll::Pending => {
-                    this.new_head = Some(new_head_stream);
+                    *this.new_head = Some(new_head_stream);
                     return Poll::Pending;
                 },
             };
