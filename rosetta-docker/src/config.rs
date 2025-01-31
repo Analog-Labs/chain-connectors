1c6b01a1ae85b00f43f867dec8875a8e01ce2ee8
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2025-01-31 05:44:51 -0300

    Fix clippy 1.81 doc craziness

--- rosetta-docker/src/config.rs
+++ rosetta-docker/src/config.rs
@@ -127,8 +127,8 @@ pub fn endpoint_from_config(config_dir: PathBuf) -> anyhow::Result<String> {
 }
 
 /// Find the Docker Endpoint of a given context, the host endpoint is located at:
-/// 
-///    UNIX: $HOME/.docker/contexts/meta/<sha256 context>/meta.json 
+///
+///    UNIX: $HOME/.docker/contexts/meta/<sha256 context>/meta.json
 /// Windows: %USERPROFILE%\.docker\contexts\meta\<sha256 context>\meta.json
 ///
 /// Is possible to list contexts by running `docker context ls`
